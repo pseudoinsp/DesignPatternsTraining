c86bacf0ae8be9ecdcf5c1b34dc5608a8109af76
pseudoinsp <gonosztaviszorny@gmail.com>
2019-03-07 11:31:28 +0100

    Visitor

---
+++ Visitor 2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor_2
+{
+    public abstract class Player
+    {
+        public abstract void Attack(Dragon dragon);
+        public abstract void Attack(Demon demon);
+    }
+
+    public class Wizard : Player
+    {
+        public override void Attack(Dragon dragon)
+        {
+            dragon.Serul(this);
+        }
+
+        public override void Attack(Demon demon)
+        {
+            demon.Serul(this);
+        }
+    }
+
+    public class Warrior : Player
+    {
+        public override void Attack(Dragon dragon)
+        {
+            dragon.Serul(this);
+        }
+
+        public override void Attack(Demon demon)
+        {
+            demon.Serul(this);
+        }
+    }
+
+    public abstract class Monster
+    {
+        protected int health = 10;
+
+        // problem: Dependency inversion
+        //          + new player impl -> every monster needs to be modified
+        //          + MOnsterHorde.Serul(Attacker a)-ra ami foreach-el a monstereken nem tud ráilleszedni
+        //public abstract void Serul(Wizard attacker);
+        //public abstract void Serul(Warrior attacker);
+
+        public abstract void Serul(Player attacker);
+    }
+
+    public class Dragon : Monster
+    {
+        // TODO FIX
+        public override void Serul(Player attacker)
+        {
+            attacker.Attack(this);
+        }
+    }
+
+    public class Demon : Monster
+    {
+        public override void Serul(Player attacker)
+        {
+            attacker.Attack(this);
+        }
+    }
+
+    public class MonsterHorde
+    {
+        public List<Monster> Monsters { get; private set; }
+
+        public MonsterHorde()
+        {
+            Monsters = new List<Monster>();
+        }
+
+        public void Serul(Player attacker)
+        {
+            foreach (var monster in Monsters)
+            {
+                monster.Serul(attacker);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wizard = new Wizard();
+
+            var horde = new MonsterHorde();
+            horde.Monsters.Add(new Dragon());
+            horde.Monsters.Add(new Demon());
+
+            horde.Serul(wizard);
+        }
+    }
+}
---
+++ Visitor 3/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor_3
+{
+    public interface ITreeVisitor
+    {
+        void Visit(StringNode node);
+        void Visit(IntNode node);
+    }
+
+    public class PreOrderVisitor : ITreeVisitor
+    {
+        public void Visit(StringNode node)
+        {
+            Console.WriteLine($"A string értéke: {node.StringValue}");
+            node.Left?.Accept(this);
+            node.Right?.Accept(this);
+        }
+
+        public void Visit(IntNode node)
+        {
+            Console.WriteLine($"A string értéke: {node.IntValue}");
+            node.Left?.Accept(this);
+            node.Right?.Accept(this);
+        }
+    }
+
+    public class InOrderVisitor : ITreeVisitor
+    {
+        public void Visit(StringNode node)
+        {
+            node.Left?.Accept(this);
+            Console.WriteLine($"A string értéke: {node.StringValue}");
+            node.Right?.Accept(this);
+        }
+
+        public void Visit(IntNode node)
+        {
+            node.Left?.Accept(this);
+            Console.WriteLine($"A string értéke: {node.IntValue}");
+            node.Right?.Accept(this);
+        }
+    }
+
+    public class PostOrderVisitor : ITreeVisitor
+    {
+        public void Visit(StringNode node)
+        {
+            node.Left?.Accept(this);
+            node.Right?.Accept(this);
+            Console.WriteLine($"A string értéke: {node.StringValue}");
+        }
+
+        public void Visit(IntNode node)
+        {
+            node.Left?.Accept(this);
+            node.Right?.Accept(this);
+            Console.WriteLine($"A string értéke: {node.IntValue}");
+        }
+    }
+
+    public abstract class Node
+    {
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public abstract void Accept(ITreeVisitor visitor);
+    }
+
+    public class StringNode : Node
+    {
+        public string StringValue { get; set; }
+        public override void Accept(ITreeVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public class IntNode : Node
+    {
+        public int IntValue { get; set; }
+        public override void Accept(ITreeVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = new StringNode()
+            {
+                StringValue = "root",
+                Left = new IntNode()
+                {
+                    IntValue = 234,
+                    Right = new StringNode()
+                    {
+                        StringValue = "jnsls"
+                    }
+                    
+                },
+                Right = new IntNode()
+                {
+                    IntValue = 21,
+                    Right = new StringNode()
+                    {
+                        StringValue = "nnre"
+                    }
+                }
+            };
+
+            var preOrderVisitor = new PreOrderVisitor();
+            n.Accept(preOrderVisitor);
+            Console.WriteLine();
+
+            var inorder = new InOrderVisitor();
+            n.Accept(inorder);
+            Console.WriteLine();
+
+            var postorder = new PostOrderVisitor();
+            n.Accept(postorder);
+
+
+            Console.ReadKey();
+        }
+    }
+}
